89a2a078860840ec925c1a0f23c6fa7df5389e25
Lars <driemtax@gmail.com>
2024-05-06 21:21:14 +0200

    Finished first imp

--- RomanToInteger.c
+++ RomanToInteger.c
@@ -2,48 +2,88 @@
 
 int romanToInt(char* s) {
     int result = 0;
-    char lastChar = '\0';
 
     int counter = 0;
-    while (s != '\0')
+    while (s[counter] != '\0')
     {
         switch (s[counter])
         {
         case 'I':
-
-            lastChar = 'I';
-            result += 1;
-            break;
-        case 'V':
-            if (lastChar = 'I')
+            if (s[counter + 1] == 'V')
             {
                 result += 4;
+                counter += 2;
             }
-            else{
-                result += 5;
+            else if (s[counter + 1] == 'X')
+            {
+                result += 9;
+                counter += 2;
             }
-            lastChar = 'V';
+            else
+            {
+                result += 1;
+                counter ++;   
+            }
+            break;
+        case 'V':
+            result += 5;
+            counter++;
             break;
         case 'X':
-            lastChar = 'X';
+            if (s[counter + 1] == 'L')
+            {
+                result += 40;
+                counter += 2;
+            }
+            else if (s[counter + 1] == 'C')
+            {
+                result += 90;
+                counter += 2;
+            }
+            else
+            {
+                result += 10;
+                counter ++;   
+            }
             break;
         case 'L':
-            lastChar = 'L';
+            result += 50;
+            counter++;
             break;
         case 'C':
-            lastChar = 'C';
+            if (s[counter + 1] == 'D')
+            {
+                result += 400;
+                counter += 2;
+            }
+            else if (s[counter + 1] == 'M')
+            {
+                result += 900;
+                counter += 2;
+            }
+            else
+            {
+                result += 100;
+                counter ++;   
+            }
             break;
         case 'D':
-            lastChar = 'D';
+            result += 500;
+            counter++;
             break;
         case 'M':
-            lastChar = 'M';
-            break;
-        default:
+            result += 1000;
+            counter++;
             break;
         }
-        counter++;
     }
-    
-    
+    return result;
+}
+
+int main(){
+    char test[] = "III";
+
+    int testResult = romanToInt(test);
+
+    printf("Test Result: %d", testResult);
 }
